51e047f6e594dbbc8caf1a8510ccc0aa696e2812
somegenericdev <95114021+somegenericdev@users.noreply.github.com>
2025-05-13 00:14:22 +0200

    fix

--- src/reverse-search-video.sh
+++ src/reverse-search-video.sh
@@ -3,14 +3,11 @@
 video_path="$1"
 tmp_dir=$(mktemp -d)
 ffmpeg -i "$video_path" -vf fps=1/10 "${tmp_dir}/img%03d.jpg" #one frame every 10 secs
-
 cd "$tmp_dir"
-
 
 search_urls_array=()
 
 for filename in *.jpg; do
-
     search_url=$(get-yandex-url.py "${tmp_dir}/${filename}")
     firefox "$search_url" &
     search_urls_array=("${search_urls_array[@]}" "$search_url")
