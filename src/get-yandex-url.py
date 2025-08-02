3ac92bbf27474801cbd2ba257b32dc44386f1c2c
somegenericdev <95114021+somegenericdev@users.noreply.github.com>
2025-08-02 04:06:43 +0200

    improved scripts

--- src/get-yandex-url.py
+++ src/get-yandex-url.py
@@ -5,7 +5,19 @@ import requests
 import sys
 
 file_path = sys.argv[1]
-response = requests.post("https://yandex.com/images/search", params={'rpt': 'imageview', 'format': 'json', 'request': '{"blocks":[{"block":"b-page_type_search-by-image__link"}]}'}, files={'upfile': ('blob', open(file_path, 'rb'), 'image/jpeg')})
-query_string = json.loads(response.content)['blocks'][0]['params']['url']
-img_search_url = 'https://yandex.com/images/search?' + query_string
+response = requests.post("https://yandex.com/images/search", params={'rpt': 'imageview', 'format': 'json',
+                                                                     'request': '{"blocks":[{"block":"b-page_type_search-by-image__link"}]}'},
+                         files={'upfile': ('blob', open(file_path, 'rb'), 'image/jpeg')})
+
+#print(response.content)
+
+img_search_url = ""
+
+try:
+    query_string = json.loads(response.content)['blocks'][0]['params']['url']
+    img_search_url = 'https://yandex.com/images/search?' + query_string
+except KeyError as e:
+    query_string = json.loads(response.content)['blocks'][0]['params']['originalImageUrl']
+    img_search_url = 'https://yandex.com/images/search?rpt=imageview&url=' + query_string
+
 print(img_search_url)
--- src/reverse-search-video.sh
+++ src/reverse-search-video.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
+set -euo pipefail
 
 video_path="$1"
+desired_searches="10"
 tmp_dir=$(mktemp -d)
-ffmpeg -i "$video_path" -vf fps=1/10 "${tmp_dir}/img%03d.jpg" #one frame every 10 secs
+
+if [[ $video_path == *.gif ]]
+then
+    echo "#####IS GIF"
+    ffmpeg -i "$video_path" "${tmp_dir}/frame_%d.jpg"
+else
+    ffmpeg -i "$video_path" "${tmp_dir}/frame_%d.jpg"
+fi
+
+
+echo "#####TMPDIR IS ${tmp_dir}"
+
 cd "$tmp_dir"
+
 
 search_urls_array=()
 
-for filename in *.jpg; do
-    search_url=$(get-yandex-url.py "${tmp_dir}/${filename}")
+
+max_index=$(ls | grep -Eo '[0-9]+' | sort -n | tail -n1)
+step=$(( max_index / desired_searches ))
+current_index="1"
+
+search_count="0"
+
+while [[ $search_count != "$desired_searches" ]]
+do
+    filename="frame_${current_index}.jpg"
+
+    for i in 1 2 3 4 5; #retry 5 times if it fails; avoids rate limiting
+    do 
+        search_url=$(get-yandex-url.py "${tmp_dir}/${filename}") && break || sleep 15
+    done
+
+
     firefox "$search_url" &
     search_urls_array=("${search_urls_array[@]}" "$search_url")
+
+    current_index=$((current_index + step))
+    search_count=$((search_count + "1"))
+
 done
+
 echo "${search_urls_array[@]}"
